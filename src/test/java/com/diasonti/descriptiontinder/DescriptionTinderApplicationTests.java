ece9257fc6c14236778d3c4b502f5f7a5883bbea
Vladimir Danilov <diasonti@gmail.com>
2019-01-28 10:00:59 +0600

    Repository setup

---
+++ src/test/java/com/diasonti/descriptiontinder/DescriptionTinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.diasonti.descriptiontinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DescriptionTinderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
