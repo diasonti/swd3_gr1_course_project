a7dd6ec3a0bd2a7e6d24289654e06f1c6a78caa4
Vladimir Danilov <diasonti@gmail.com>
2019-03-10 21:22:52 +0600

    Rework anonymous component

--- front/src/components/Anonymous.vue
+++ front/src/components/Anonymous.vue
@@ -1,39 +1,56 @@
 <template>
+
     <div class="container-fluid">
-        <div class="col-6">
-            <form>
-                <div class="form-group">
-                    <label for="logInUsernameInput">Username</label>
-                    <input v-model="logInUsername" type="text" class="form-control" id="logInUsernameInput"
-                           placeholder="Enter username" @keyup.enter="submitLogIn">
-                </div>
-                <div class="form-group">
-                    <label for="logInPasswordInput">Password</label>
-                    <input v-model="logInPassword" type="password" class="form-control" id="logInPasswordInput"
-                           placeholder="Enter password" @keyup.enter="submitLogIn">
-                </div>
-                <button @click="submitLogIn" type="button" class="btn btn-primary">Sign in</button>
-            </form>
-        </div>
-        <div class="col-6">
-            <form>
-                <div class="form-group">
-                    <label for="registerUsernameInput">Username</label>
-                    <input v-model="registerUsername" type="text" class="form-control" id="registerUsernameInput"
-                           placeholder="Enter username">
-                </div>
-                <div class="form-group">
-                    <label for="registerPasswordInput1">Password</label>
-                    <input v-model="registerPassword1" type="password" class="form-control" id="registerPasswordInput1"
-                           placeholder="Enter password">
-                </div>
-                <div class="form-group">
-                    <label for="registerPasswordInput2">Confirm password</label>
-                    <input v-model="registerPassword2" type="password" class="form-control" id="registerPasswordInput2"
-                           placeholder="Enter password again">
-                </div>
-                <button @click="submitRegistration" type="button" class="btn btn-primary">Sign up</button>
-            </form>
+        <div class="row">
+            <div class="col"></div>
+
+
+            <div class="col" v-if="currentTab === 'login'">
+                <h2>Sign in</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="logInUsernameInput">Username</label>
+                        <input v-model="logInUsername" type="text" class="form-control" id="logInUsernameInput"
+                               placeholder="Enter username" @keyup.enter="submitLogIn">
+                    </div>
+                    <div class="form-group">
+                        <label for="logInPasswordInput">Password</label>
+                        <input v-model="logInPassword" type="password" class="form-control" id="logInPasswordInput"
+                               placeholder="Enter password" @keyup.enter="submitLogIn">
+                    </div>
+                    <button @click="submitLogIn" type="button" class="btn btn-primary">Sign in</button>
+                    <br/>
+                    <a @click="currentTab = 'register'" href="#">Don't have an account yet?</a>
+                </form>
+            </div>
+
+
+            <div class="col" v-if="currentTab === 'register'">
+                <h2>Sign up</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="registerUsernameInput">Username</label>
+                        <input v-model="registerUsername" type="text" class="form-control no-autocomplete" id="registerUsernameInput"
+                               placeholder="Enter username" @keyup.enter="submitRegistration" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="registerPasswordInput1">Password</label>
+                        <input v-model="registerPassword1" type="password" class="form-control no-autocomplete" id="registerPasswordInput1"
+                               placeholder="Enter password" @keyup.enter="submitRegistration" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="registerPasswordInput2">Confirm password</label>
+                        <input v-model="registerPassword2" type="password" class="form-control no-autocomplete" id="registerPasswordInput2"
+                               placeholder="Enter password again" @keyup.enter="submitRegistration" readonly>
+                    </div>
+                    <button @click="submitRegistration" type="button" class="btn btn-primary">Sign up</button>
+                    <br/>
+                    <a @click="currentTab = 'login'" href="#">Already have an account?</a>
+                </form>
+            </div>
+
+
+            <div class="col"></div>
         </div>
     </div>
 </template>
@@ -44,6 +61,7 @@
         props: [],
         data() {
             return {
+                currentTab: 'login',
                 logInUsername: '',
                 logInPassword: '',
                 logInFailed: false,
@@ -89,6 +107,12 @@
             if(this.$store.getters.token) {
                 this.$router.replace('/profile');
             }
+
+            // Workaround to prevent form autocomplete
+            setTimeout(function () {
+                const inputs = $(".no-autocomplete");
+                inputs.attr('readonly', false);
+            }, 100);
         }
     }
 </script>
--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-	
+    <footer>
+
+    </footer>
 </template>
 
 <script>
