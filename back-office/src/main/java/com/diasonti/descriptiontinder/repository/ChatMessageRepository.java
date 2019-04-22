8388765c4afd0dd53bc772ea3f103b3ad5bc8468
Vladimir Danilov <diasonti@gmail.com>
2019-04-22 10:39:46 +0600

    Fix another matches' messages in chat

--- back-office/src/main/java/com/diasonti/descriptiontinder/repository/ChatMessageRepository.java
+++ back-office/src/main/java/com/diasonti/descriptiontinder/repository/ChatMessageRepository.java
@@ -2,13 +2,17 @@ package com.diasonti.descriptiontinder.repository;
 
 import com.diasonti.descriptiontinder.data.entity.ChatMessage;
 import com.diasonti.descriptiontinder.data.entity.MmMatch;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ChatMessageRepository extends PagingAndSortingRepository<ChatMessage, Long> {
 
     Optional<ChatMessage> findTopByMatchOrderBySentAtDesc(MmMatch match);
+
+    List<ChatMessage> findByMatch(MmMatch match, Pageable page);
 }
